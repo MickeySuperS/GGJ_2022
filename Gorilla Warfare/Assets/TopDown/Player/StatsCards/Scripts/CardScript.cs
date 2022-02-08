19cb182f0a015a26484270c6a460d9b42dc048cb
MickeySuperS <mickeysupers@gmail.com>
2022-02-08 03:57:21 +0200

    powerup ui beside health bar

--- Gorilla Warfare/Assets/TopDown/Player/StatsCards/Scripts/CardScript.cs
+++ Gorilla Warfare/Assets/TopDown/Player/StatsCards/Scripts/CardScript.cs
@@ -19,6 +19,8 @@ namespace TopDown
 
         Action callback;
 
+        public bool interactable = true;
+
 
         public void SetupCard(string nameText, string detailText, Sprite sprite, bool isRare, Action callback)
         {
@@ -32,19 +34,32 @@ namespace TopDown
             this.callback = callback;
         }
 
+        public void SetupCard(CardScript card)
+        {
+            OnPointerExit(null);
+
+            interactable = false;
+            spriteUI.color = card.spriteUI.color;
+            rareUI.SetActive(card.rareUI.activeSelf);
+            nameMainText.text = "";
+            detailsText.text = "";
+            spriteUI.sprite = card.spriteUI.sprite;
+            this.callback = null;
+        }
 
         public void OnPointerEnter(PointerEventData eventData)
         {
+            if (!interactable) return;
             transform.localScale = Vector3.one * 1.2f;
             foreach (var item in displayHoverText)
             {
                 item.SetActive(true);
             }
-
         }
 
         public void OnPointerExit(PointerEventData eventData)
         {
+            if (!interactable) return;
             transform.localScale = Vector3.one;
             foreach (var item in displayHoverText)
             {
@@ -54,6 +69,7 @@ namespace TopDown
 
         public void OnPointerClick(PointerEventData eventData)
         {
+            if (!interactable) return;
             callback?.Invoke();
         }
 
--- Gorilla Warfare/Assets/TopDown/World/Scripts/PowerUpsManager.cs
+++ Gorilla Warfare/Assets/TopDown/World/Scripts/PowerUpsManager.cs
@@ -21,6 +21,8 @@ namespace TopDown
 
         public Player targetPlayer;
 
+        public Transform powerUpUIParent;
+
 
         List<PowerUpScriptableObject> powerUpsList = new List<PowerUpScriptableObject>();
 
@@ -34,63 +36,71 @@ namespace TopDown
                 int rand = UnityEngine.Random.Range(0, powerUpsList.Count);
                 bool isRare = UnityEngine.Random.Range(0f, 1000f) > 900f;
                 var powerUp = powerUpsList[rand];
-                card.SetupCard(powerUp.mainText, isRare ? powerUp.rareDetailsText : powerUp.detailText, powerUp.sprite, isRare, GetCallback(powerUp.powerupType, isRare));
+                card.SetupCard(powerUp.mainText, isRare ? powerUp.rareDetailsText : powerUp.detailText, powerUp.sprite, isRare, GetCallback(powerUp.powerupType, isRare, card));
                 powerUpsList.RemoveAt(rand);
             }
             powerUpCanvas.gameObject.SetActive(true);
             GamePause.instance.PauseGame(false);
         }
 
-        public System.Action GetCallback(PowerUps powerUp, bool isRare)
+        void CreateUICard(CardScript card)
+        {
+            var newCardUI = Instantiate(powerUpUIParent.GetChild(0).gameObject, powerUpUIParent, false);
+            newCardUI.SetActive(true);
+            newCardUI.GetComponent<CardScript>().SetupCard(card);
+
+        }
+
+        public System.Action GetCallback(PowerUps powerUp, bool isRare, CardScript card)
         {
             switch (powerUp)
             {
                 case PowerUps.AttackDamange:
                     if (isRare)
                     {
-                        return () => { targetPlayer.hController.damageValue += 30; targetPlayer.gController.damageValue += 60; GamePause.instance.ResumeGame(); powerUpCanvas.gameObject.SetActive(false); };
+                        return () => { CreateUICard(card); targetPlayer.hController.damageValue += 30; targetPlayer.gController.damageValue += 60; GamePause.instance.ResumeGame(); powerUpCanvas.gameObject.SetActive(false); };
                     }
                     else
                     {
-                        return () => { targetPlayer.hController.damageValue += 10; targetPlayer.gController.damageValue += 20; GamePause.instance.ResumeGame(); powerUpCanvas.gameObject.SetActive(false); };
+                        return () => { CreateUICard(card); targetPlayer.hController.damageValue += 10; targetPlayer.gController.damageValue += 20; GamePause.instance.ResumeGame(); powerUpCanvas.gameObject.SetActive(false); };
                     }
 
                 case PowerUps.AttackSpeed:
                     if (isRare)
                     {
-                        return () => { targetPlayer.hController.attackCD -= 0.3f; GamePause.instance.ResumeGame(); powerUpCanvas.gameObject.SetActive(false); };
+                        return () => { CreateUICard(card); targetPlayer.hController.attackCD -= 0.3f; GamePause.instance.ResumeGame(); powerUpCanvas.gameObject.SetActive(false); };
                     }
                     else
                     {
-                        return () => { targetPlayer.hController.attackCD -= 0.2f; GamePause.instance.ResumeGame(); powerUpCanvas.gameObject.SetActive(false); };
+                        return () => { CreateUICard(card); targetPlayer.hController.attackCD -= 0.2f; GamePause.instance.ResumeGame(); powerUpCanvas.gameObject.SetActive(false); };
                     }
                 case PowerUps.Heart:
                     if (isRare)
                     {
-                        return () => { targetPlayer.GetComponent<PlayerHealth>().SetMaxHealth(2500f); GamePause.instance.ResumeGame(); powerUpCanvas.gameObject.SetActive(false); };
+                        return () => { CreateUICard(card); targetPlayer.GetComponent<PlayerHealth>().SetMaxHealth(2500f); GamePause.instance.ResumeGame(); powerUpCanvas.gameObject.SetActive(false); };
                     }
                     else
                     {
-                        return () => { targetPlayer.GetComponent<PlayerHealth>().SetMaxHealth(1000f); GamePause.instance.ResumeGame(); powerUpCanvas.gameObject.SetActive(false); };
+                        return () => { CreateUICard(card); targetPlayer.GetComponent<PlayerHealth>().SetMaxHealth(1000f); GamePause.instance.ResumeGame(); powerUpCanvas.gameObject.SetActive(false); };
                     }
                 case PowerUps.MoveSpeed:
                     if (isRare)
                     {
-                        return () => { targetPlayer.hController.playerSpeed += 4f; GamePause.instance.ResumeGame(); powerUpCanvas.gameObject.SetActive(false); };
+                        return () => { CreateUICard(card); targetPlayer.hController.playerSpeed += 4f; GamePause.instance.ResumeGame(); powerUpCanvas.gameObject.SetActive(false); };
                     }
                     else
                     {
-                        return () => { targetPlayer.hController.playerSpeed += 2f; GamePause.instance.ResumeGame(); powerUpCanvas.gameObject.SetActive(false); };
+                        return () => { CreateUICard(card); targetPlayer.hController.playerSpeed += 2f; GamePause.instance.ResumeGame(); powerUpCanvas.gameObject.SetActive(false); };
                     }
 
                 case PowerUps.Shield:
                     if (isRare)
                     {
-                        return () => { targetPlayer.hController.damageFactor -= 0.5f; targetPlayer.gController.damageFactor -= 0.5f; GamePause.instance.ResumeGame(); powerUpCanvas.gameObject.SetActive(false); };
+                        return () => { CreateUICard(card); targetPlayer.hController.damageFactor -= 0.5f; targetPlayer.gController.damageFactor -= 0.5f; GamePause.instance.ResumeGame(); powerUpCanvas.gameObject.SetActive(false); };
                     }
                     else
                     {
-                        return () => { targetPlayer.hController.damageFactor -= 0.25f; targetPlayer.gController.damageFactor -= 0.25f; GamePause.instance.ResumeGame(); powerUpCanvas.gameObject.SetActive(false); };
+                        return () => { CreateUICard(card); targetPlayer.hController.damageFactor -= 0.25f; targetPlayer.gController.damageFactor -= 0.25f; GamePause.instance.ResumeGame(); powerUpCanvas.gameObject.SetActive(false); };
                     }
 
 
